e42794b79540fed8017f48df5f97ebf118b55c91
Lucas Masiero <lucas.masi.ero@hotmail.com>
2023-10-23 14:47:54 -0300

    feat: video-linter

---
+++ 05-Go-Testing/Video-Linter/script/code/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"errors"
+	"math"
+)
+
+func main() {
+
+}
+
+// calculator.go
+var (
+	// DivisionError is returned when a division by zero is attempted.
+	DivisionError = errors.New("division by zero")
+	// ErrCalculatorSquareRoot is returned when a square root of a negative number is attempted.
+	ErrCalculatorSquareRoot = errors.New("Square root of negative number.")
+)
+
+// Calculator provides methods to perform simple calculations.
+type Calculator struct{}
+
+// Add returns the sum of two integers.
+func (c *Calculator) Add(a, b float64) (result float64) {
+	result = a + b
+	return
+}
+
+// Subtract returns the difference of two integers.
+func (c *Calculator) Subtract(a, b float64) (result float64) {
+	result = a - b
+	return
+}
+
+// Divide returns the quotient of two integers.
+func (c *Calculator) Divide(a, b float64) (result float64, err error) {
+	if b == 0 {
+		err = DivisionError
+		return
+	}
+	result = a / b
+	return
+}
+
+// SquareRoot returns the square root of an integer.
+func (c *Calculator) SquareRoot(a float64) (result float64, err error) {
+	if a < 0 {
+		err = ErrCalculatorSquareRoot
+		return
+	}
+	result = math.Sqrt(a)
+	return
+}
